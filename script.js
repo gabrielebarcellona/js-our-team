95fc041749d44e4e2045bfecf09420193190d326
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-04-28 17:03:45 +0200

    add object stampato in pagina

--- script.js
+++ script.js
@@ -23,40 +23,45 @@ Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg*/
 
+    const EleList = document.querySelector('.output')
+     
+
 
 const members = [
     {
     nomeECognome : 'wayne Barnett',
     Ruolo : 'Founder & CEO',
     foto : 'wayne-barnett-founder-ceo.jpg',
-    }
+    },
     {
     nomeECognome : 'Angela Caroll',
     Ruolo : 'Chief Editor',
     foto : 'angela-caroll-chief-editor.jpg',
-    }
+    },
     {
     nomeECognome : 'Walter Gordon',
     Ruolo : 'Office Manager',
     foto : 'walter-gordon-office-manager.jpg'
-    }
+    },
     {
     nomeECognome : 'Angela Lopez',
     Ruolo : 'Social Media Manager',
     foto : 'angela-lopez-social-media-manager.jpg'
-    }
+    },
     {
     nomeECognome : 'Scott Estrada',
     Ruolo : 'Developer',
     foto : 'scott-estrada-developer.jpg'
-    }  
+    } ,
     {
     nomeECognome : 'Barbara Ramos',
     Ruolo : 'Graphic Designer',
     foto : 'barbara-ramos-graphic-designer.jpg'
     }
 ]
-
-for(i = 0 ; i < members.length ; i++){
     console.log(members)
-}
+
+    for (i = 0 ; i < members.length ; i ++){
+        let member = members[i];
+    EleList.innerHTML += `<li>${member.nomeECognome} ${member.ruolo}${ member.foto}</li>`
+    }
